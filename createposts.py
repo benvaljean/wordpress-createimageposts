b5dd861aca1b055211456e8aa9f6e7db3779fe69
Benjamin Goodacre <f1@goodacre.name>
2015-10-07 01:45:55 +0100

    Replaced relevant print statements with logger

--- createposts.py
+++ createposts.py
@@ -4,9 +4,14 @@
 
 import ConfigParser
 import os, itertools, datetime, time, sys, getopt
+
 from PIL import Image
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 version = "1.9.12"
 
@@ -23,14 +28,14 @@ def updatesite(site):
 				titlelistdir[p.strip()] = p.lower().replace(' ','-').strip()
 	if not titlelistdir:
 		#all dirs
-		print 'all titles set'
+		logger.info('all titles set')
 		titlelist = open(config.get(site, 'TitlesList'),'r')
 		for line in titlelist.readlines():
 			titlelistdir[line.strip()] = line.lower().replace(' ','-').strip()
 	for title in titlelistdir:
 		dir = config.get(site, 'MediaRoot') + titlelistdir[title]
 		mtime = lambda f : datetime.datetime.fromtimestamp(os.path.getmtime(dir + '/' + f))
-		print title + ' ' + dir
+		logger.info(title + ' ' + dir)
 		#tuple 4=by the hour  3=by the day
 		mtime_hour = lambda f: datetime.datetime(*mtime(f).timetuple()[:4])
 		dir_files = sorted(os.listdir(dir), key=mtime)
@@ -39,9 +44,9 @@ def updatesite(site):
 			dir_files = filter(lambda f: datetime.datetime.strptime(lastrun,'%Y-%m-%d %H:%M:%S') < mtime(f), dir_files)
 		#dir_files = filter(lambda f: datetime.datetime(2010,1,1,0) <	mtime(f) < datetime.datetime(2015,1,1,0), dir_files)
 		by_hour = dict((k,list(v)) for k,v in itertools.groupby(dir_files, key=mtime_hour)) #python 2.6
-		print by_hour
+		logger.info(by_hour)
 		for f in by_hour:
-			print f
+			logger.warning(f)
 			post = WordPressPost()
 			post.title = title
 			post.terms_names = {
@@ -53,14 +58,14 @@ def updatesite(site):
 	
 			if os.path.isfile(tagsfilename):
 				taglist = open(tagsfilename,'r')
-				print tagsfilename
+				logger.debug(tagsfilename)
 				for l in taglist.readlines():
 					post.terms_names['post_tag'].append(l.strip())
 			if os.path.isfile(catsfilename):
 				catlist = open(catsfilename,'r')
 				for li in catlist.readlines():
 					post.terms_names['category'].append(li.strip())
-			print post.terms_names
+			logger.debug(post.terms_names)
 			post.content = ''
 			for pic in by_hour[f]:
 	#		   if pic.lower().endswith(('.jpg','.jpeg','.png','.gif')):
@@ -75,11 +80,11 @@ def updatesite(site):
 							height =''
 							pass
 						entry='<a href="' + urlroot + titlelistdir[title] + '/' + pic + '"><img class="alignnone size-medium tooltips" title="' + str(width) + ' x ' + str(height) + '" src="' + urlroot + titlelistdir[title] + '/' + fileName + '-150x150' + fileExtension + '"></a>'
-						#print entry
+						logger.debug(entry)
 						post.content += entry
 	
 			post.content = post.content.strip()
-			print post.content
+			logger.debug(post.content)
 			pattern = '%Y-%m-%d %H:%M:%S'
 	#	   epoch = int(time.mktime(time.strptime(f, pattern)))
 	
@@ -93,7 +98,7 @@ def updatesite(site):
 			wp.call(NewPost(post))
 	
 	if doallpics == 0:
-		print 'Newpost appears added OK, updating lastrun'
+		logger.info('Newpost appears added OK, updating lastrun')
 		with open (lastrunfile, "w") as myfile:
 			myfile.write(time.strftime("%Y-%m-%d %H:%M:%S"))
 	
@@ -101,7 +106,7 @@ def updatesite(site):
 try:
 	opts,remainder = getopt.getopt(sys.argv[1:],'t:vs:d:h',['title=','version','site=','date=','help'])
 except getopt.GetoptError as err:
-	print str(err)
+	logger.error(str(err))
 	showhelp()
 
 scriptdir = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -120,28 +125,28 @@ if len(sys.argv) == 1:
 else:
 	for o,p in opts:
 		if o in ['-d','--date']:
-			print 'we have date set'
+			logger.info('we have date set')
 			if p == 'all':
 				doallpics = 1
 			elif p == 'inc':
 				doallpics = 0
 			else:
 				showhelp()
-				print "Syntax error: --date takes argument 'all' or 'inc'"
+				logger.error("Syntax error: --date takes argument 'all' or 'inc'")
 				sys.exit(1)
 		elif o in ['-s','--site']:
 			if p == 'all':
 				site='all'
 			else:
 				if p not in config.sections():
-					print p + ' section not found in config file ' + configfile
+					logger.error(p + ' section not found in config file ' + configfile)
 					sys.exit(1)
 				site=p
 		elif o in ['-v','--version','--help']:
 			showhelp()
 			sys.exit(0)
 
-print 'site is' + site
+logger.info('site is' + site)
 
 if doallpics == 0:
 	#read from the lasttun file and set the var
@@ -149,7 +154,7 @@ if doallpics == 0:
 	if os.path.isfile(lastrunfile):
 		with open (lastrunfile, "r") as myfile:
 			lastrun=myfile.read().replace('\n', '')
-		print "last run " + lastrun
+		logger.info("last run " + lastrun)
 	else:
 		#section exists but not lastrun file - presume doallpics = 1
 		d = raw_input('Lastrun file not found, continuing with all pics anyway? Press <Enter> to continue of Ctrl-C to exit')
@@ -157,8 +162,8 @@ if doallpics == 0:
 	
 if site == 'all':
 	for each_section in config.sections():
-		print each_section
-		print config.get(each_section, 'WP-User')
+		logger.info(each_section)
+		logger.info(config.get(each_section, 'WP-User'))
 		updatesite(each_section)
 else:
 	updatesite(site)
